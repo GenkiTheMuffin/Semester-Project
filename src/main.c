f32fde84816c5b05f5d294efd8b78e26137a05ca
r0b0ti <roboti.vigh@gmail.com>
2025-12-12 11:57:52 +0100

    ..

--- src/main.c
+++ src/main.c
@@ -25,16 +25,16 @@ unsigned int adc_value;
 float voltage, speed;
 
 // variable to check if car is in specific state
-int start = 0;
-int section1 = 0;
-int section2 = 0;
+bool start = false;
+bool section1 = true;
+bool section2 = false;
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int main(void) {
   init();
   int page = 0; // int page = 0;
-  int distance1 = 1, distance2 = 3, time1 = 3, time2 = 38, duty = 100;
+  int distance1 = 1, distance2 = 3, time1 = 3, time2 = 5, duty = 100;
   float progressbar = 0;
   float total_distance = 0;
   float needed_speed_1, needed_speed_2, current_speed;
@@ -44,86 +44,30 @@ int main(void) {
   int *pDuty = &duty;
 
   while (1) {
-
+    
     // Read voltage; ADC conversion //
     voltage = measure_volt_adc();
 
     // Updates the display //
-    update_nextion(&page, &distance1, &distance2, &time1, &time2, &progressbar,
-                   &total_distance);
-    needed_speed_1 = (float)distance1 / time1;
-    needed_speed_2 = (float)distance2 / time2;
+    update_nextion(&page, &distance1, &distance2, &time1, &time2, &progressbar, &total_distance);
+    needed_speed_1 = (float)distance1/time1;
+    needed_speed_2 = (float)distance2/time2;
 
-    while (page == 1) {
+
+    while(page == 1) {
       // Execute //
-      pwm1_set_duty(*pDuty); // sets the motor voltage to the required value
-      *pCurrent_speed = measure_speed(time_value); // gets the current speed
+      pwm1_set_duty(*pDuty);                          // sets the motor voltage to the required value
+      *pCurrent_speed = measure_speed(time_value);    // gets the current speed
+      
+    if (section1) {
+      active_speed_control(pNeeded_speed_1, pCurrent_speed, pDuty, 5);  // sets speed according to section 1
 
-      if (section1) {
-        active_speed_control(pNeeded_speed_1, pCurrent_speed, pDuty,
-                             5); // sets speed according to section 1
+    } else if (section2) {
+      active_speed_control(pNeeded_speed_2, pCurrent_speed, pDuty, 5);  // sets speed according to section 2
 
-      } else if (section2) {
-        active_speed_control(pNeeded_speed_2, pCurrent_speed, pDuty,
-                             5); // sets speed according to section 2
-
-      } else {
-        pwm1_set_duty(0); // stops the car
-      }
-
-      if (total_distance >= distance1 &&
-          section1) { // switches from section 1 to section 2
-        section1 = false;
-        section2 = true;
-      }
-      if (total_distance >=
-          (distance1 + distance2)) { // resets the sections and start
-        section1 = false;
-        section2 = false;
-        page = 0;
-        total_distance = 0;
-        progressbar = 0;
-        pwm1_set_duty(0); // stops the car
-        printf("page 0%c%c%c", 255, 255, 255);
-      }
-      // Measure time //
-      time_value = get_enc_period() /
-                   1000; // gets encoder wheel time output in milliseconds
-
-      // Speed measurement //
-      speed = measure_speed(time_value);
-
-      // Update distance //
-      update_current_distance(&total_distance); // updates the total taken
-                                                // distance until this moment
-
-      // Updates the display //
-      // update_nextion(&page, &distance1, &distance2, &time1, &time2,
-      // &progressbar, &total_distance);
-
-      progressbar +=
-          (int)((total_distance / (distance1 + distance2)) *
-                100); // calculates the progress % based on the distance
-
-      printf("%f", needed_speed_1);
-      printf("page1.n0.val=%d%c%c%c", *pDuty, 255, 255, 255);
-      printf("page1.pb1.val=%d%c%c%c", (int)progressbar, 255, 255, 255);
-      printf("page1.x0.vvs1=%d%c%c%c", 2, 255, 255, 255);
-      printf("page1.x1.vvs1=%d%c%c%c", 2, 255, 255, 255);
-      printf("page1.x2.vvs1=%d%c%c%c", 2, 255, 255, 255);
-      printf("page1.x3.vvs1=%d%c%c%c", 2, 255, 255, 255);
-
-      printf("page1.x0.val=%d%c%c%c", (int)(total_distance * 100), 255, 255,
-             255);
-      printf("page1.x1.val=%d%c%c%c", (int)(*pNeeded_speed_2 * 100), 255, 255,
-             255);
-      printf("page1.x2.val=%d%c%c%c", (int)(*pCurrent_speed * 100), 255, 255,
-             255);
-      printf("page1.x3.val=%d%c%c%c", (int)(*pNeeded_speed_1 * 100), 255, 255,
-             255);
+    } else {
+      pwm1_set_duty(0);   // stops the car
     }
-<<<<<<< HEAD
-=======
 
     if (total_distance >= distance1 && section1) { // switches from section 1 to section 2
       section1 = false;
@@ -151,7 +95,7 @@ int main(void) {
     // Updates the display //
     //update_nextion(&page, &distance1, &distance2, &time1, &time2, &progressbar, &total_distance);
     
-    progressbar += (int)((total_distance / (distance1 + distance2)) * 100);   // calculates the progress % based on the distance
+    progressbar = (int)((total_distance / (distance1 + distance2)) * 100);   // calculates the progress % based on the distance
 
 
 
@@ -171,7 +115,6 @@ int main(void) {
 
     }
     
->>>>>>> 65993ef7782b9656c2270a31aa2924fb8e3fe8ed
   }
   return 0;
 }
